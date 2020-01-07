3581a059d657d2b8d1503fe55e06d0b735775318
Manassés G. Soares <manasses.soares7@gmail.com>
2020-01-07 18:41:57 -0300

    Update letsencrypt_install.sh

--- bin/letsencrypt_install.sh
+++ bin/letsencrypt_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# File  : letsencrypt_install.sh
+
 
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
 /opt/letsencrypt/letsencrypt-auto certonly --standalone --email $1 --agree-tos --no-eff-email -d $2
